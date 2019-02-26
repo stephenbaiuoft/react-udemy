bf4da7ab6e1ce8ff2267e47d6df4e741326a26fb
stephen bai <stephenbai91@gmail.com>
2019-02-26 05:40:35 -0500

    Creating Subdoc Collections

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,9 +1,25 @@
+const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
+
+// pull up the Survey schema from mongoose
+const Survey = mongoose.model('surveys');
 
 // default ES6 of exporting javascript
 module.exports = (app) => {
     app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
+        // read key values from req ES6 syntax
+       const { title, subject, body, recipients } = req.body; 
         
+       const survey = new Survey({
+            title: title, 
+            subject, // abbreviated ES6 syntax
+            body, 
+            recipients: recipients.split(',').map(email => {  // split and return an array of objects
+                return {email: email}; 
+            }),
+            _user: req.user.id,
+            dateSent: Date.now()
+       });
     });
 };
