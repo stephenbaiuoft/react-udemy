ca166adbbd88262eae57feeb3943eaafe2c83723
stephen bai <stephenbai91@gmail.com>
2019-03-04 22:26:57 -0500

    add button

--- client/src/Components/Dashboard.js
+++ client/src/Components/Dashboard.js
@@ -3,7 +3,11 @@ import React from 'react'; // bable converts ES6 to CommonJS under the hood
 const Dashboard = () => {
     return (
         <div>
-            Dashboard Component
+            Dashboard Component   
+            <div className="fixed-action-btn">
+                <a class="btn-floating btn-large waves-effect waves-light red">
+                <i class="material-icons">add</i></a>        
+            </div>
         </div>
     );
 };
