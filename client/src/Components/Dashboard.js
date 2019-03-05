dd90dd225020671e76945b9f086de3096f7fda27
stephen bai <stephenbai91@gmail.com>
2019-03-04 22:32:19 -0500

    asdf

--- client/src/Components/Dashboard.js
+++ client/src/Components/Dashboard.js
@@ -1,12 +1,14 @@
 import React from 'react'; // bable converts ES6 to CommonJS under the hood
+import { Link } from 'react-router-dom'; // Link tag
 
 const Dashboard = () => {
     return (
         <div>
             Dashboard Component   
             <div className="fixed-action-btn">
-                <a class="btn-floating btn-large waves-effect waves-light red">
-                <i class="material-icons">add</i></a>        
+                <Link to= "/surveys/new" class="btn-floating btn-large waves-effect waves-light red">
+                    <i class="material-icons">add</i>
+                </Link>        
             </div>
         </div>
     );
