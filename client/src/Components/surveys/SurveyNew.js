0dd04bf6f7b180c801b3b92d4cfc0a9c035e9e0e
stephen bai <stephenbai91@gmail.com>
2019-03-05 09:37:07 -0500

    redux-form and added form:reduxForm auth

---
+++ client/src/Components/surveys/SurveyNew.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class SurveyNew extends Component {
+    render() {
+        return (
+            <div>
+                SurveyNew Form Class
+            </div>
+
+        );
+
+    }
+}
+
+
+export default SurveyNew; 
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,10 +1,13 @@
 import {combineReducers} from 'redux';
+import { reducer as reduxForm } from 'redux-form';
 import authReducer from './authReducer'; // default import
+
 
 // reducers that are tied to the store variable through
 // this combineReducers method
 export default combineReducers(
     {
-       auth: authReducer    
+       auth: authReducer,  
+       form: reduxForm // reduxForm has to use form as the key to connect
     }
 );
